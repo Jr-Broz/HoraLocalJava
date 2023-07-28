8f0fc66dcdf31d5987ad025a4fa2d94d48f76a2a
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-07-28 16:33:51 -0300

    comitando em java

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args){ 
+
+        Date relogio = new Date();
+
+    System.out.println("a hora do sistema é");
+    System.out.println(relogio.toString());
+
+
+    }
+}
+
+// sempre que utilizar a palavra new está criando um novo objeto
